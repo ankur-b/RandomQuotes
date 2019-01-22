c1600c761854ade07bbcd409c645410b1f1aba8e
ankur-b <barveankur29@outlook.com>
2019-01-22 22:50:15 +0530

    Updates

---
+++ precache-manifest.ac5a6494b684dcf49fa535ec54fa1ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ce52d4091dc647c73cb",
+    "url": "/random-quotes/static/css/main.53a23c47.chunk.css"
+  },
+  {
+    "revision": "9ce52d4091dc647c73cb",
+    "url": "/random-quotes/static/js/main.9ce52d40.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/random-quotes/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a4554ae4c421f369a667",
+    "url": "/random-quotes/static/js/runtime~main.a4554ae4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quotes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe862c69970177581833a2e6b455c561",
+    "url": "/random-quotes/index.html"
+  }
+];
